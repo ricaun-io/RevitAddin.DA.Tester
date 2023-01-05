efaa4c394e2b8fe21688a70e69085db3b1346336
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-01-05 18:03:50 -0300

    Update summary

--- DesignAutomationConsole/Services/PageUtils.cs
+++ DesignAutomationConsole/Services/PageUtils.cs
@@ -7,6 +7,13 @@ namespace DesignAutomationConsole.Services
 {
     public static class PageUtils
     {
+        /// <summary>
+        /// Retrieves a list of items of type T by calling the provided pageGetter function repeatedly
+        /// until all pages have been retrieved.
+        /// </summary>
+        /// <typeparam name="T">The type of the items being retrieved.</typeparam>
+        /// <param name="pageGetter">A function that takes a pagination token and returns a page of items of type T.</param>
+        /// <returns>A list of items of type T.</returns>
         public static async Task<List<T>> GetAllItems<T>(Func<string, Task<Page<T>>> pageGetter)
         {
             var ret = new List<T>();
@@ -21,6 +28,15 @@ namespace DesignAutomationConsole.Services
             return ret;
         }
 
+        /// <summary>
+        /// Retrieves a list of items of type T by calling the provided pageGetter function repeatedly
+        /// until all pages have been retrieved.
+        /// </summary>
+        /// <typeparam name="T">The type of the items being retrieved.</typeparam>
+        /// <typeparam name="P">The type of the parameter being passed to the pageGetter function.</typeparam>
+        /// <param name="pageGetter">A function that takes a parameter of type P and a pagination token, and returns a page of items of type T.</param>
+        /// <param name="parameter">The parameter to be passed to the pageGetter function.</param>
+        /// <returns>A list of items of type T.</returns>
         public static async Task<List<T>> GetAllItems<T, P>(Func<P, string, Task<Page<T>>> pageGetter, P parameter)
         {
             var ret = new List<T>();
